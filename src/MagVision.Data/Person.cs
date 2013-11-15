e579ff1c5c7f60b02966c86cf46b68cdd3575657
Daniel Wonisch <daniel.wonisch@gmail.com>
2013-11-15 16:55:14 +0100

    Added missing file

---
+++ src/MagVision.Data/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagVision.Data
+{
+    public class Person
+    {
+        public Person()
+        {
+            PhoneNumbers = new List<PhoneNumber>();
+        }
+
+        public List<PhoneNumber> PhoneNumbers { get; set; }
+    }
+}
